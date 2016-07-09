b6e1279cf875d93fd24abd3b01832fb03f914f38
Nicolette Chambers <nicolette3883@gmail.com>
2016-07-09 15:01:09 -0500

    #2 | Refactor Application Class to take menux as a parameter, and call it when application starts

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,15 @@
 public class Application {
     private final ItemCatalog itemCatalog;
+    private final MenuX menux;
 
-    public Application(ItemCatalog itemCatalog) {
+    public Application(ItemCatalog itemCatalog, MenuX menux) {
 
         this.itemCatalog = itemCatalog;
+        this.menux = menux;
     }
 
     public void start() {
         itemCatalog.list();
+        menux.display();
     }
 }
--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -9,10 +9,23 @@ public class ApplicationTest {
     @Test
     public void shouldDisplayAllAvailableItemsInTheGroceryStoreWhenApplicationStarts () {
         ItemCatalog itemCatalog = mock(ItemCatalog.class);
-        Application application = new Application(itemCatalog);
+        MenuX menux = mock(MenuX.class);
+        Application application = new Application(itemCatalog, menux);
 
         application.start();
 
         verify(itemCatalog).list();
     }
+
+    @Test
+    public void shouldDisplayMenuOptionWhenApplicationStarts() {
+        ItemCatalog itemCatalog = mock(ItemCatalog.class);
+        MenuX menux = mock(MenuX.class);
+        Application application = new Application(itemCatalog, menux);
+
+        application.start();
+
+        verify(menux).display();
+    }
 }
+
