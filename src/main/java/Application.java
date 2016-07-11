152beb81127e7ccb153ca15f390903f0387c473e
Nicolette Chambers <nicolette3883@gmail.com>
2016-07-11 11:19:03 -0500

    #5 | Display Welcome Message When Application Starts

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,18 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class Application {
     private final Menu menu;
     private final MenuValidator menuValidator;
     private final BufferedReader bufferedReader;
+    private final PrintStream printStream;
 
-    public Application(Menu menu, MenuValidator menuValidator, BufferedReader bufferedReader) {
+    public Application(Menu menu, MenuValidator menuValidator, BufferedReader bufferedReader, PrintStream printStream) {
         this.menu = menu;
         this.menuValidator = menuValidator;
         this.bufferedReader = bufferedReader;
+        this.printStream = printStream;
     }
 
     public void start() {
+        printStream.println("Welcome to Grocery Dash!");
         menu.display();
         String userInput = getUserInput();
         menuValidator.validate(userInput);
